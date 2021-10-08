143c00426fb1b856d318ceca81c83b9c5b4b84f9
Ted Clayton <ted_clayton@hotmail.com>
2021-10-08 15:01:57 -0400

    Init

---
+++ problem_solving_one.py
@@ -0,0 +1,16 @@
+# input data hello from user
+# print out in reverse
+
+
+# greeting = input("Please say hello: ")
+# stringlength=len(greeting)
+# slicedString=greeting[stringlength::-1]
+# print(slicedString)
+
+#input data string from user
+# Print out the first letter of each word as capital with a space in between 
+
+conversation = input("What are you doing today and please respond in lowercase letters? ")
+result = conversation.title()
+
+print(result)
