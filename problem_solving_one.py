6549e0f633b943926480b69e17f98512b020f0e2
Ted Clayton <ted_clayton@hotmail.com>
2021-10-08 17:16:42 -0400

    The first problem involved the users input printed in reverse.

--- problem_solving_one.py
+++ problem_solving_one.py
@@ -10,7 +10,21 @@
 #input data string from user
 # Print out the first letter of each word as capital with a space in between 
 
-conversation = input("What are you doing today and please respond in lowercase letters? ")
-result = conversation.title()
+# conversation = input("What are you doing today and please respond in lowercase letters? ")
+# result = conversation.title()
 
-print(result)
+# print(result)
+
+#input data string 
+# and then compress that string
+new_string = ""
+string = "olyoxandfreeeeeeee2223333322221111554998"
+count = 1
+for i in range(len(string)-1):
+    if string[i] == string[i+1]:
+        count = count + 1
+    else:
+        new_string = new_string + string[i] + str(count)
+        count = 1
+new_string = new_string + string[i+1] + str(count)
+print(new_string)
